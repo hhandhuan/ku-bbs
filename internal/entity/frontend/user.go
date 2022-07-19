462aa08dfbaf2f8412865b07fe8e91f6463e0e62
hhandhuan <95403191+hhandhuan@users.noreply.github.com>
2022-07-19 22:53:43 +0800

    fix: 修复用户名正则匹配错误

--- internal/entity/frontend/user.go
+++ internal/entity/frontend/user.go
@@ -3,12 +3,12 @@ package frontend
 import "github.com/hhandhuan/ku-bbs/internal/model"
 
 type LoginReq struct {
-	Name     string `v:"required|regex:[\u4e00-\u9fa5a-zA-Z0-9]{2,8}#用户名错误|用户名格式错误" form:"name"`
+	Name     string `v:"required|regex:^[\u4e00-\u9fa5a-zA-Z0-9]{2,8}$#用户名错误|用户名格式错误" form:"name"`
 	Password string `v:"required|length:6,20#账号或密码错误|密码长度错误" form:"password"`
 }
 
 type RegisterReq struct {
-	Name            string `v:"required|regex:[\u4e00-\u9fa5a-zA-Z0-9]{2,8}#用户名错误|用户名格式错误" form:"name"`
+	Name            string `v:"required|regex:^[\u4e00-\u9fa5a-zA-Z0-9]{2,8}$#用户名错误|用户名格式错误" form:"name"`
 	Password        string `v:"required|length:6,20|same:confirm_password#账号或密码错误|密码长度错误|密码和确认密码不一致" form:"password"`
 	ConfirmPassword string `v:"required|length:6,20#确认密码错误|密码长度错误" form:"confirm_password"`
 }
