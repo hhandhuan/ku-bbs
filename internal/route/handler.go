d89df9cbd1e6316aa36d4412b40b44067cfa3f6f
hhandhuan <95403191+hhandhuan@users.noreply.github.com>
2022-09-07 14:12:52 +0800

    chore: 添加鉴权中间件

--- internal/route/handler.go
+++ internal/route/handler.go
@@ -6,9 +6,18 @@ import (
 )
 
 func isAdmin(ctx *gin.Context) {
-	s := srv.Context(ctx)
-	if !s.IsAdmin() {
+	if s := srv.Context(ctx); !s.IsAdmin() {
 		s.To("/").WithError("无权限访问").Redirect()
+		ctx.Abort()
+		return
+	} else {
+		ctx.Next()
+	}
+}
+
+func IsAuth(ctx *gin.Context) {
+	if s := srv.Context(ctx); !s.Check() {
+		s.To("/login").WithError("请登录后，再继续操作").Redirect()
 		ctx.Abort()
 		return
 	} else {
