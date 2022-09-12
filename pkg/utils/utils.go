bed6cde0a45a32be90a22b9dbbedc1513c5d9ebe
hhandhuan <95403191+hhandhuan@users.noreply.github.com>
2022-09-12 11:13:29 +0800

    chore: 添加闪存提交数据，防止表单数据校验错误数据丢失问题

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hhandhuan/ku-bbs/pkg/utils/time"
 	"github.com/hhandhuan/ku-bbs/pkg/utils/view"
 	"html/template"
+	"reflect"
 )
 
 // GetTemplateFuncMap 获取模版函数
@@ -16,4 +17,15 @@ func GetTemplateFuncMap() template.FuncMap {
 		"RemindName":       view.RemindName,
 		"StrLimit":         str.Limit,
 	}
+}
+
+// StructToMap 结构体转换成字典
+func StructToMap(s interface{}) map[string]interface{} {
+	t := reflect.TypeOf(s)
+	v := reflect.ValueOf(s)
+	data := make(map[string]interface{})
+	for i := 0; i < t.NumField(); i++ {
+		data[t.Field(i).Name] = v.Field(i).Interface()
+	}
+	return data
 }
