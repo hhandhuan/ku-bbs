46cf2430aa755f3d351a669d5ac371e2dcb82cdf
hhandhuan <95403191+hhandhuan@users.noreply.github.com>
2022-10-20 10:34:44 +0800

    chore: 优化样式和修复已知问题

--- internal/service/frontend/comment.go
+++ internal/service/frontend/comment.go
@@ -152,12 +152,12 @@ func (s *SComment) Delete(id uint64) error {
 		if d.Error != nil || d.RowsAffected <= 0 {
 			return fmt.Errorf("delete comment error: %v", d.Error)
 		}
-		u := tx.Model(&model.Topics{}).Where("id", comment.TopicId).Updates(map[string]interface{}{
-			"comment_count": gorm.Expr("comment_count - 1"),
-		})
-		if u.Error != nil || u.RowsAffected <= 0 {
-			return fmt.Errorf("delete comment error: %v", d.Error)
-		}
+		//u := tx.Model(&model.Topics{}).Where("id", comment.TopicId).Updates(map[string]interface{}{
+		//	"comment_count": gorm.Expr("comment_count - 1"),
+		//})
+		//if u.Error != nil || u.RowsAffected <= 0 {
+		//	return fmt.Errorf("delete comment error: %v", d.Error)
+		//}
 		return nil
 	})
 
